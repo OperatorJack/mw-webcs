0a4e37c97f7768226a703831d651edc5f0c596db
OperatorJack <thepaladinof@gmail.com>
2020-09-24 13:45:49 -0400

    Updates

---
+++ precache-manifest.7ef07ca87265c96cadce89aee7ed1ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e443b5d7d22085c4d7d1b2800f84d358",
+    "url": "/mw-webcs/index.html"
+  },
+  {
+    "revision": "bb79601c37ec853e575b",
+    "url": "/mw-webcs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8950f1432492271396a",
+    "url": "/mw-webcs/static/js/2.a9929053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mw-webcs/static/js/2.a9929053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb79601c37ec853e575b",
+    "url": "/mw-webcs/static/js/main.70a70eb3.chunk.js"
+  },
+  {
+    "revision": "a4bde339c53251600bbe",
+    "url": "/mw-webcs/static/js/runtime-main.97b833fd.js"
+  }
+]);
